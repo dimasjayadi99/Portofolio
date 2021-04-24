f3f84a26c673d1917080ea731a0de265e83a6246
Dimas Jayadi <dimasjayadi7@gmail.com>
2021-04-24 13:27:13 +0700

    My portofolio

---
+++ index.php
@@ -0,0 +1,237 @@
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="mystyle.css" type="text/css"> 
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <title>Welcome back to my page</title>
+  </head>
+  <body>
+
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-muted">
+            <div class="container">
+            <a class="navbar-brand font-weight-bold" href="#">Portofolio</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">About</a>
+                <a class="nav-item nav-link" href="#">Progress</a>
+                <a class="nav-item nav-link" href="#">Design</a>
+                <a class="nav-item nav-link" href="#">Contact</a>
+                </div>
+            </div>
+            </div>
+        </nav>
+
+        <!-- Jumbotron -->
+
+        <div class="jumbotron jumbotron-fluid text-light">
+            <div class="container">
+              <div data-aos="zoom-in-up">
+                <h1 class="display-4 font-weight-bold">HI, WELCOME TO MY PAGE,<br>I'M DIMAS.</h1>
+                <p class="lead">Developer living and learning in Jakarta, Indonesia.<img src="location.svg"><p>I'm passionate about creating engaging apps, experiences and I want to use technology to help make people's lives more fulfilling. You can find me on Linkedin and Github , download my resume or shoot me an email.</p></p>
+              </div>  
+            </div>
+        </div>
+
+         <!--  -->
+         <h3 class="txg text-center">What Can I Do</h3>
+         <p class="text-center">I'm passionate about creating engaging apps, design<br>and many more with technology. <a href="">Click here for more detail</a></p>
+            <div class="container">
+                <div class="row row-cols-3">
+                        <div class="col"><img src="ui.svg"><p><p class="lead2 font-weight-bold">Make Design<p>We make designs interesting accordingly client need</div>
+                        <div class="col"><img src="ui.svg"><p><p class="lead2 font-weight-bold">Design to Code<p>We create clean code with pre-made designs</div>
+                        <div class="col"><img src="ui.svg"><p><p class="lead2 font-weight-bold">Dynamic Apps<p>We create dynamic applications according to the latest data</div>
+                          <div class="col"><img src="ui.svg"><p><p class="lead2 font-weight-bold">Dynamic Apps<p>We create dynamic applications according to the latest data</div>
+                </div>
+            </div>
+
+        <!--  -->
+        <hr>
+        <h3 class="txg text-center">My Progress</h3>
+        <p class="text-center">This is my progress of programming language, <br>framework and database. <a href="">Click here for more detail</a></p>
+
+       <!-- progress -->
+            <div class="container">
+                <div class="progress-me">
+                  <ul> 
+                        
+                  <li>HTML & CSS
+                      <div class="progress">
+                              <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">80%</div>
+                      </div>
+                  </li>
+
+                  <li>JavaScript
+                      <div class="progress">
+                              <div class="progress-bar" role="progressbar" style="width: 50%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">50%</div>
+                      </div>
+                  </li>
+
+                  <li>PHP
+                      <div class="progress">
+                              <div class="progress-bar" role="progressbar" style="width: 70%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">70%</div>
+                      </div>
+                  </li>
+
+                  <li>Java
+                          <div class="progress">
+                                  <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">90%</div>
+                          </div>
+                  </li>
+
+                  </ul>
+              </div>
+          </div>
+        <!-- end progress -->
+
+        <!-- progress -->
+      <div class="container">
+              <h4>Framework</h4>
+              <ul> 
+                    
+              <li>Bootstrap
+                  <div class="progress">
+                          <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">80%</div>
+                  </div>
+              </li>
+
+              <li>CodeIgniter
+                  <div class="progress">
+                          <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">80%</div>
+                  </div>
+              </li>
+              
+              <li>Spring Framework
+                  <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">60%</div>
+                  </div>
+              </li>
+
+              </ul>
+      </div>
+    <!-- end progress -->
+
+            <!-- progress -->
+      <div class="container">
+              <h4>Database</h4>
+              <ul> 
+                    
+              <li>Firebase
+                  <div class="progress">
+                          <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">80%</div>
+                  </div>
+              </li>
+
+              <li>MySQL
+                  <div class="progress">
+                          <div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">60%</div>
+                  </div>
+              </li>
+              
+              <li>PostrgreSQL
+                  <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 75%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">75%</div>
+                  </div>
+              </li>
+
+              </ul>
+      </div>
+    <!-- end progress -->
+
+
+    <!-- card 2 -->
+      <hr>
+      <div data-aos="fade-up">
+      <h3 class="txg text-center">My Design</h3>
+      <p class="text-center">This is my collection of design. <a href="">Click here for more detail</a></p>
+      </div>
+      <div class="container">
+      <div class="work">
+        <div class="card-group">
+            
+            <div class="card">
+              <div data-aos="fade-right">
+                  <img src="design1.png" class="card-img-top" alt="...">
+                      <div class="card-body">
+                            <h5 class="card-title">ShowRoom</h5>
+                            <p class="card-text">
+                            <ul>
+                              <li>Landing Page</li>
+                              <li>Category  : Gallery</li>
+                              <li>Make with : Adobe XD</li>
+                              <li>Size      : 1366 x 786</li>
+                              <li>Cover     : 1920 x 1080</li>
+                            </ul>
+                            </p>
+                      </div>
+              </div>
+            
+              <button type="button" class="btn btn-outline-primary">Show Detail</button>
+
+              <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </div>
+            
+            </div>
+
+            <div class="card">
+              <div data-aos="fade-left">
+                  <img src="design2.png" class="card-img-top" alt="...">
+                      <div class="card-body">
+                            <h5 class="card-title">ShowRoom</h5>
+                            <p class="card-text">
+                            <ul>
+                              <li>Landing Page</li>
+                              <li>Category  : Gallery</li>
+                              <li>Make with : Adobe XD</li>
+                              <li>Size      : 1366 x 786</li>
+                              <li>Cover     : 1920 x 1080</li>
+                            </ul>
+                            </p>
+                      </div>
+              </div>
+            
+              <button type="button" class="btn btn-outline-primary">Show Detail</button>
+
+              <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+              </div>
+            
+            </div>
+
+          </div></div></div>
+
+            <!-- end card 2 -->
+
+            <!-- Footer -->
+
+            <footer>
+            <div class="row text-center text-light">
+              <div class="col-md-4 copy"><img src="facebook.png"><img src="instagram.png"><img src="twitter.png"></div>
+              <div class="col-md-4 copy"><p>Copyright © 2021 - Dimas Jayadi</p></div>
+              <div class="col-md-4"></div>
+            </div>
+            </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+     AOS.init();
+    </script>
+
+  </body>
+</html>
