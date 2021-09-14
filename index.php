f171a6ecb58609fd9b1bb9340eab6c42ae8e2982
Dimas Jayadi <61482156+dimasjayadi99@users.noreply.github.com>
2021-09-14 23:44:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="mystyle.css" type="text/css"> 
+    <link rel="stylesheet" href="css/style.css" type="text/css"> 
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
     <title>Welcome back to my page</title>
